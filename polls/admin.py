3d6bceb7bd575e1653b7b7f10438fd421ef73278
mint <estefanyazocar3@gmail.com>
2022-03-14 12:28:24 -0500

    cambios

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from .models import Choice, Question
-# ...
-class QuestionAdmin(admin.ModelAdmin):
-    # ...
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
+from .models import Question, Choice
+
+admin.site.register(Question)
+admin.site.register(Choice)
